64e2d4946b2e38e4acac84c9ccec33e869bd4816
Joseph Gordy <jgordy2424@gmail.com>
2017-09-25 17:10:52 -0400

    getting data from the fetch

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 export default class ShowPost extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  fetch('https://tiny-lasagna-server.herokuapp.com/collections/blogger/').then(results => {
+        return results.json();
+      }).then(data => {
+        // this.setState({songs: data});
+        console.log(data);
+      })
+}
+
   render() {
     return (
       <div className="ShowPost">
